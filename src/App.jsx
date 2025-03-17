b35b5f17f4849f1926fb0e0ea6f4e11cb97e3391
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-03-17 21:05:39 +0530

    17 march

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
 import Title from "./Components/Title/Title";
+import About from "./Components/About/About";
+import Campus from "./Components/Campus/Campus";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
 
 const App = () => {
   return (
@@ -12,6 +16,13 @@ const App = () => {
       <div className="container">
         <Title subtitle="Our Program" title="What We Offer" />
         <Programs />
+        <About />
+        <Title subtitle="Gallery" title="Campus  Photos" />
+        <Campus />
+        <Title subtitle="Testomonials" title="What Student Says" />
+        <Testimonials />
+        <Title subtitle="Contact Us" title="Get in Touch" />
+        <Contact />
       </div>
     </div>
   );

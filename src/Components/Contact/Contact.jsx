b3abdeda11414e1a2450c64a444b534a76e314ec
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-03-18 20:20:34 +0530

    made it respnsive

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -7,8 +7,34 @@ import mail_icon from "../../assets/mail-icon.png";
 import phone_icon from "../../assets/phone-icon.png";
 
 import location_icon from "../../assets/location-icon.png";
+import white_arrow from "../../assets/white-arrow.png";
 
 const Contact = () => {
+  const [result, setResult] = React.useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "eab9bb1e-5121-4b93-9d35-c8bd0888ba98");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData,
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
   return (
     <div className="contact">
       <div className="contact-col">
@@ -41,9 +67,33 @@ const Contact = () => {
         </ul>
       </div>
       <div className="contact-col">
-        <form>
+        <form onSubmit={onSubmit}>
           <label>Your Name</label>
+          <input
+            type="text"
+            name="name"
+            placeholder="Enter your name "
+            required
+          />
+          <label>Phone Number</label>
+          <input
+            type="tel"
+            name="phone"
+            placeholder="Enter your mobile number"
+            required
+          />
+          <label>Write your messages here</label>
+          <textarea
+            name="message"
+            rows="6"
+            placeholder="Enter your message "
+            required
+          ></textarea>
+          <button type="submit" className="btn dark-btn">
+            Submit Now <img src={white_arrow} alt="" />
+          </button>
         </form>
+        <span>{result}</span>
       </div>
     </div>
   );

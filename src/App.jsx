75bd57adccb706ede48da6e3336f7d147e9df1ab
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-03-19 10:09:45 +0530

    Edusity completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import Programs from "./Components/Programs/Programs";
@@ -8,8 +8,10 @@ import Campus from "./Components/Campus/Campus";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
+import Videoplayer from "./Components/VideoPlayer/Videoplayer";
 
 const App = () => {
+  const [playState, setPlayState] = useState(false);
   return (
     <div>
       <Navbar />
@@ -17,7 +19,7 @@ const App = () => {
       <div className="container">
         <Title subtitle="Our Program" title="What We Offer" />
         <Programs />
-        <About />
+        <About setPlayState={setPlayState} />
         <Title subtitle="Gallery" title="Campus  Photos" />
         <Campus />
         <Title subtitle="Testomonials" title="What Student Says" />
@@ -26,6 +28,7 @@ const App = () => {
         <Contact />
         <Footer />
       </div>
+      <Videoplayer playState={playState} setPlayState={setPlayState} />
     </div>
   );
 };

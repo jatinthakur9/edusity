32cd5803684d8fe95ba8af77c76f6f8b1f6c9868
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-05-20 23:17:10 +0530

    h

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Campus from "./Components/Campus/Campus";
 import Testimonials from "./Components/Testimonials/Testimonials";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
-import Videoplayer from "./Components/VideoPlayer/VideoPlayer";
+import Videoplayer from "./Components/VideoPlayer/Videoplayer";
 const App = () => {
   const [playState, setPlayState] = useState(false);
   return (

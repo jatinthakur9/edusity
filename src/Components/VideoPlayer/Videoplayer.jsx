05ea333d93adb2147ce3d034f6465513d491793e
jatin thakur <97386157+jatinthakur9@users.noreply.github.com>
2025-05-20 23:04:58 +0530

    Y

--- src/Components/VideoPlayer/Videoplayer.jsx
+++ src/Components/VideoPlayer/Videoplayer.jsx
@@ -1,9 +1,9 @@
 import React, { useRef } from "react";
 
-import "./Videoplayer.css";
+import "./VideoPlayer.css";
 import video from "../../assets/coll-video.mp4";
 
-const Videoplayer = ({ playState, setPlayState }) => {
+const VideoPlayer = ({ playState, setPlayState }) => {
   const player = useRef(null);
 
   const closePlayer = (e) => {
@@ -22,4 +22,4 @@ const Videoplayer = ({ playState, setPlayState }) => {
   );
 };
 
-export default Videoplayer;
+export default VideoPlayer;
